1c7cc6469a47536485252cc62aa905e75480c775
Enrique Aviña Mejia <enrique.amejia97@gmail.com>
2020-07-06 19:39:16 -0500

    backend folder created, and repositories added

---
+++ lib/backend/repositories/user_repository.dart
@@ -0,0 +1,14 @@
+import '../models/user.dart';
+
+abstract class UserRepository {
+  User add(User newUser);
+  User update(User userToUpdate);
+  bool remove(int userId);
+  User find(int userId);
+  List<User> findAll();
+  List<User> findByFollower(int userId);
+  List<User> findByComment(int commentId);
+  List<User> findByLike(int likeId);
+  List<User> findByPost(int postId);
+  List<User> findFollowers();
+}
